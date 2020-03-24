71e44d108440aea33bfa2586df0f048eecb3e206
Tomasz Klim <github@tomaszklim.pl>
2020-03-24 16:40:33 +0100

    renaming sf-farm-manager extension

--- cron/inspect.sh
+++ cron/inspect.sh
@@ -7,8 +7,8 @@ SSH=/opt/farm/ext/binary-ssh-client/wrapper/ssh
 sshkey=`/opt/farm/ext/keys/get-ssh-device-key.sh mikrotik`
 for router in `cat /etc/local/.farm/mikrotik.hosts |grep -v ^#`; do
 
-	host=`/opt/farm/ext/farm-manager/internal/decode.sh host $router`
-	port=`/opt/farm/ext/farm-manager/internal/decode.sh port $router`
+	host=`/opt/farm/mgr/farm-manager/internal/decode.sh host $router`
+	port=`/opt/farm/mgr/farm-manager/internal/decode.sh port $router`
 
 	$SSH -y -i $sshkey -p $port -o StrictHostKeyChecking=no admin@$host export \
 		|/opt/farm/ext/versioning/save.sh daily 20 /var/cache/farm mikrotik-$host.config
@@ -20,8 +20,8 @@ done
 sshkey=`/opt/farm/ext/keys/get-ssh-device-key.sh cisco`
 for router in `cat /etc/local/.farm/cisco.hosts |grep -v ^#`; do
 
-	host=`/opt/farm/ext/farm-manager/internal/decode.sh host $router`
-	port=`/opt/farm/ext/farm-manager/internal/decode.sh port $router`
+	host=`/opt/farm/mgr/farm-manager/internal/decode.sh host $router`
+	port=`/opt/farm/mgr/farm-manager/internal/decode.sh port $router`
 
 	$SSH -y -i $sshkey -p $port -o StrictHostKeyChecking=no admin@$host "show running-config" \
 		|/opt/farm/ext/versioning/save.sh daily 20 /var/cache/farm cisco-$host.config
@@ -35,8 +35,8 @@ done
 sshkey=`/opt/farm/ext/keys/get-ssh-device-key.sh usg`
 for router in `cat /etc/local/.farm/usg.hosts |grep -v ^#`; do
 
-	host=`/opt/farm/ext/farm-manager/internal/decode.sh host $router`
-	port=`/opt/farm/ext/farm-manager/internal/decode.sh port $router`
+	host=`/opt/farm/mgr/farm-manager/internal/decode.sh host $router`
+	port=`/opt/farm/mgr/farm-manager/internal/decode.sh port $router`
 
 	$SSH -y -i $sshkey -p $port -o StrictHostKeyChecking=no admin@$host "mca-ctrl -t dump-cfg" \
 		|/opt/farm/ext/versioning/save.sh daily 20 /var/cache/farm usg-$host.json
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /opt/farm/scripts/setup/extension.sh sf-versioning
-/opt/farm/scripts/setup/extension.sh sf-farm-manager
+/opt/farm/scripts/setup/extension.sh sm-farm-manager
 /opt/farm/scripts/setup/extension.sh sf-binary-ssh-client
 
 echo "setting up base directories and files"
