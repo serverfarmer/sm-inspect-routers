1e118392b81039947bf025966494103d1e136ea0
Tomasz Klim <github@tomaszklim.pl>
2021-11-28 11:37:53 +0100

    moving inventory files to new directory

--- cron/inspect.sh
+++ cron/inspect.sh
@@ -5,40 +5,40 @@ SSH=/opt/farm/ext/binary-ssh-client/wrapper/ssh
 
 # http://fajne.it/automatyzacja-backupu-routera-mikrotik.html
 sshkey=`/opt/farm/ext/keys/get-ssh-device-key.sh mikrotik`
-for router in `cat ~/.farm/mikrotik.hosts |grep -v ^#`; do
+for router in `grep -v ^# ~/.serverfarmer/inventory/mikrotik.hosts`; do
 
 	host=`/opt/farm/mgr/farm-manager/internal/decode.sh host $router`
 	port=`/opt/farm/mgr/farm-manager/internal/decode.sh port $router`
 
 	$SSH -y -i $sshkey -p $port -o StrictHostKeyChecking=no admin@$host export \
-		|/opt/farm/ext/versioning/save.sh daily 20 /var/cache/farm mikrotik-$host.config
+		|/opt/farm/ext/versioning/save.sh daily 20 ~/.serverfarmer/inspection mikrotik-$host.config
 
 done
 
 
 # https://supportforums.cisco.com/document/110946/ssh-using-public-key-authentication-ios-and-big-outputs
 sshkey=`/opt/farm/ext/keys/get-ssh-device-key.sh cisco`
-for router in `cat ~/.farm/cisco.hosts |grep -v ^#`; do
+for router in `grep -v ^# ~/.serverfarmer/inventory/cisco.hosts`; do
 
 	host=`/opt/farm/mgr/farm-manager/internal/decode.sh host $router`
 	port=`/opt/farm/mgr/farm-manager/internal/decode.sh port $router`
 
 	$SSH -y -i $sshkey -p $port -o StrictHostKeyChecking=no admin@$host "show running-config" \
-		|/opt/farm/ext/versioning/save.sh daily 20 /var/cache/farm cisco-$host.config
+		|/opt/farm/ext/versioning/save.sh daily 20 ~/.serverfarmer/inspection cisco-$host.config
 
 	$SSH -y -i $sshkey -p $port -o StrictHostKeyChecking=no admin@$host "show tech-support" \
-		|/opt/farm/ext/versioning/save.sh daily 20 /var/cache/farm cisco-$host.tech
+		|/opt/farm/ext/versioning/save.sh daily 20 ~/.serverfarmer/inspection cisco-$host.tech
 
 done
 
 
 sshkey=`/opt/farm/ext/keys/get-ssh-device-key.sh usg`
-for router in `cat ~/.farm/usg.hosts |grep -v ^#`; do
+for router in `grep -v ^# ~/.serverfarmer/inventory/usg.hosts`; do
 
 	host=`/opt/farm/mgr/farm-manager/internal/decode.sh host $router`
 	port=`/opt/farm/mgr/farm-manager/internal/decode.sh port $router`
 
 	$SSH -y -i $sshkey -p $port -o StrictHostKeyChecking=no admin@$host "mca-ctrl -t dump-cfg" \
-		|/opt/farm/ext/versioning/save.sh daily 20 /var/cache/farm usg-$host.json
+		|/opt/farm/ext/versioning/save.sh daily 20 ~/.serverfarmer/inspection usg-$host.json
 
 done
--- setup.sh
+++ setup.sh
@@ -5,14 +5,13 @@
 /opt/farm/scripts/setup/extension.sh sf-binary-ssh-client
 
 echo "setting up base directories and files"
-mkdir -p   ~/.farm /var/cache/farm
+mkdir -p ~/.serverfarmer/inspection ~/.serverfarmer/inventory
+chmod 0710 ~/.serverfarmer ~/.serverfarmer/inspection
+chown root:www-data ~/.serverfarmer ~/.serverfarmer/inspection
 
-touch      ~/.farm/cisco.hosts ~/.farm/mikrotik.hosts ~/.farm/usg.hosts
-chmod 0600 ~/.farm/cisco.hosts ~/.farm/mikrotik.hosts ~/.farm/usg.hosts
-chmod 0700 ~/.farm
-
-chmod 0710 /var/cache/farm
-chown root:www-data /var/cache/farm
+touch      ~/.serverfarmer/inventory/cisco.hosts ~/.serverfarmer/inventory/mikrotik.hosts ~/.serverfarmer/inventory/usg.hosts
+chmod 0600 ~/.serverfarmer/inventory/cisco.hosts ~/.serverfarmer/inventory/mikrotik.hosts ~/.serverfarmer/inventory/usg.hosts
+chmod 0700 ~/.serverfarmer/inventory
 
 if ! grep -q /opt/farm/mgr/inspect-routers/cron /etc/crontab; then
 	echo "49 6 * * * root /opt/farm/mgr/inspect-routers/cron/inspect.sh" >>/etc/crontab
